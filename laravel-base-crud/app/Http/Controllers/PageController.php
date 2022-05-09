ca3aec63bbb30b0ddeb41c0d77ae1015618e6917
camillapalombi <camilla.palombi14@gmail.com>
2022-05-09 16:29:04 +0200

    Creazione tabella nel database

--- laravel-base-crud/app/Http/Controllers/PageController.php
+++ laravel-base-crud/app/Http/Controllers/PageController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comic;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        
+        $myComics = Comic::paginate(10);
+        
+        $data = [
+            'comics' => $myComics
+        ];
+
+        return view('index', $data);
+    }
 }
---
+++ laravel-base-crud/database/migrations/2022_05_09_133723_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 70);
+            $table->text('description', 800);
+            $table->char('thumb', 250);
+            $table->float('price', 8, 2);
+            $table->char('series', 200);
+            $table->date('date');
+            $table->char('type',100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
