d6c7164d818d9918c04b349477a8b5e05c4c55cf
camillapalombi <camilla.palombi14@gmail.com>
2022-05-11 16:35:52 +0200

    Migliorato aspetto delle card

--- laravel-base-crud/resources/views/comics/index.blade.php
+++ laravel-base-crud/resources/views/comics/index.blade.php
@@ -4,15 +4,37 @@
 
 @section('pageMain')
 
-@foreach ($comics as $comic)
-                <div class="single-card">
-                    <ul>
-                        <li> <h3> {{ $comic->title }} </h3> </li>
-                        <li> {{ $comic->description }}</li> 
-                    </ul>
-                </div>
-@endforeach
+<div class="container">
 
+    <div class="row g-5">
+
+        @foreach ($comics as $comic)
+
+        <div class="col-4">
+
+            <div class="card h-100">
+
+                <img class="comics-thumbs" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                <h3> <a class="title-link" href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a> </h3>
+                <div>{{ $comic->description }}</div>
+                <div> <h5>Prezzo:</h5> {{ $comic->price }}$ </div>
+                <div><h5>Serie:</h5> {{ $comic->series }}</div>
+                <div><h5>Data:</h5> {{ $comic->date }}</div>
+                <div><h5>Tipo:</h5> {{ $comic->type }}</div>
+                
+            </div>
+
+        </div>
+    
+        @endforeach
+    </div>
+
+</div>
+
+
+<!--PAGINAZIONE -->
 {{ $comics->links() }}
+
+
 @endsection
 
