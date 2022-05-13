fb6df4d935620e376d3019b495846aef47b22d7a
camillapalombi <camilla.palombi14@gmail.com>
2022-05-13 15:52:47 +0200

    Inizio implementazione EDIT

---
+++ laravel-base-crud/resources/views/comics/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('templates.base')
+
+@section('pageTitle', 'Edit comics listing')
+
+@section('pageMain')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form method="POST" action="{{ route('comics.update', $comic->id ) }}">
+
+                    @csrf
+                    @method('PUT')
+
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Image</label>
+                        <input type="text" class="form-control" id="image" name="thumb" value="{{ $comic->thumb }}">
+                      </div>
+
+                    <div class="mb-3">
+                      <label for="title" class="form-label">Title</label>
+                      <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Series</label>
+                        <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="Type" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="Type" name="type" value="{{ $comic->type }}">
+                    </div>
+                   
+                    <div class="mb-3">
+                        <label for="date" class="form-label">Date</label>
+                        <input type="date" class="form-control" id="date" name="date" value="{{ $comic->date }}">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea class="form-control" id="description" rows="3" name="description"> {{ $comic->description }} </textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- laravel-base-crud/resources/views/partials/footer.blade.php
+++ laravel-base-crud/resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="">
+<footer>
 
     <!--FOOTER TOP-->
   <div class="footer-top container">
